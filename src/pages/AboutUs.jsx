34cc3250b5e18bc9131d9fab12461cedc0c5fb80
ayatbd <albehestione@gmail.com>
2024-03-01 00:29:06 +0600

    updated popular page and some other components

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,45 @@
+/* eslint-disable react/no-unescaped-entities */
+import about_image from "../../src/assets/images/about_us.jpg";
 const AboutUs = () => {
-  return <div></div>;
+  return (
+    <div>
+      <section className="bg-gray-100">
+        <div className="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
+            <div className="max-w-lg">
+              <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+                About Us
+              </h2>
+              <p className="mt-4 text-gray-600 text-lg">
+                Discover a world of joy and imagination in our about section! At
+                our toy website, we're passionate about creating smiles. Dive
+                into our story, where every toy is a tale waiting to be told.
+                Learn about our commitment to quality, safety, and the magical
+                journey that brings laughter to children's faces. Welcome to the
+                enchanting realm of play!
+              </p>
+              <div className="mt-8">
+                <a
+                  href="#"
+                  className="text-blue-500 hover:text-blue-600 font-medium"
+                >
+                  Learn more about us
+                  <span className="ml-2">&#8594;</span>
+                </a>
+              </div>
+            </div>
+            <div className="mt-12 md:mt-0">
+              <img
+                src={about_image}
+                alt="About Us Image"
+                className="object-cover rounded-lg shadow-md"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default AboutUs;
--- src/pages/home/PopularToys.jsx
+++ src/pages/home/PopularToys.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { FaRegStar, FaStar } from "react-icons/fa";
 import Rating from "react-rating";
+import "../../../src/index.css";
 
 const PopularToys = () => {
   const [toys, setToys] = useState([]);
@@ -24,45 +25,44 @@ const PopularToys = () => {
       <h2 className="text-4xl text-center font-bold mb-8">Popular in Store</h2>
       <main className="grid grid-cols-2 gap-x-6 gap-y-10 px-2 pb-20 sm:grid-cols-3 sm:px-8 lg:mt-16 lg:grid-cols-4 lg:gap-x-4 lg:px-0">
         {sortedByRatings.slice(0, 4).map((toy) => (
-          <article key={toy.id} className="relative border">
-            <div className="aspect-square overflow-hidden border">
-              <img
-                className="h-full w-full object-cover transition-all duration-300 group-hover:scale-125"
-                src={toy.photo}
-                alt=""
-              />
-            </div>
-            <div className="mt-4 flex items-start justify-between">
-              <div className="">
-                <h3 className="text-xs font-semibold sm:text-sm md:text-base">
-                  <a href="#" title="" className="">
-                    {toy.toyname}
-                    <span className="absolute" aria-hidden="true"></span>
-                  </a>
-                </h3>
-                <div className="flex items-center gap-3 mt-1">
-                  <Rating
-                    placeholderRating={toy.rating}
-                    readonly
-                    emptySymbol={<FaRegStar />}
-                    placeholderSymbol={<FaStar className="text-warning" />}
-                    fullSymbol={<FaStar />}
-                  />
-                  <p className="font-[500]">{toy.rating} /5</p>
+          <div key={toy.id} className="relative border">
+            <div className="main-div relative">
+              <div className="aspect-square overflow-hidden border">
+                <img
+                  className="h-full w-full object-cover transition-all duration-300 group-hover:scale-125"
+                  src={toy.photo}
+                  alt=""
+                />
+              </div>
+              <div className="mt-4 flex items-start justify-between px-2">
+                <div className="">
+                  <h3 className="text-xs font-semibold sm:text-sm md:text-base">
+                    <a href="#" title="" className="">
+                      {toy.toyname}
+                      <span className="absolute" aria-hidden="true"></span>
+                    </a>
+                  </h3>
+                  <div className="flex items-center gap-3 mt-1">
+                    <Rating
+                      placeholderRating={toy.rating}
+                      readonly
+                      emptySymbol={<FaRegStar />}
+                      placeholderSymbol={<FaStar className="text-warning" />}
+                      fullSymbol={<FaStar />}
+                    />
+                    <p className="font-[500]">{toy.rating} /5</p>
+                  </div>
+                </div>
+
+                <div className="text-right">
+                  <p className="text-xs font-normal sm:text-sm md:text-base">
+                    $ {toy.price}.00
+                  </p>
                 </div>
               </div>
-
-              <div className="text-right">
-                <del className="mt-px text-xs font-semibold text-gray-600 sm:text-sm">
-                  {" "}
-                  $79.00{" "}
-                </del>
-                <p className="text-xs font-normal sm:text-sm md:text-base">
-                  $99.00
-                </p>
-              </div>
             </div>
-          </article>
+            <div className="on-hover-anim absolute top-0 left-0 w-full h-full bg-gray-800 z-50"></div>
+          </div>
         ))}
       </main>
     </div>
